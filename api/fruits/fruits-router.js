0cb3c8b77fb0cdff6890aa1f01b01c8f25ca77d4
ashleyannlaz <paperpastels@gmail.com>
2021-09-07 18:15:51 -0400

    Db

--- api/fruits/fruits-router.js
+++ api/fruits/fruits-router.js
@@ -1,51 +1,33 @@
-const express = require('express');
-const knex = require('knex');
-
-const db = knex({
-  client: 'sqlite3',
-  connection: {
-    filename: './data/produce.db3'
-  },
-  useNullAsDefault: true
-});
+const express = require("express");
+const Fruit = require("./fruits-model");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  db('fruits')
-    .then(fruits => {
-      res.json(fruits);
-    })
-    .catch(err => {
-      res.status(500).json({ message: 'Failed to retrieve fruits' });
-    });
+router.get("/", async (req, res, next) => {
+  try {
+    const all = await Fruit.getAll();
+    res.json(all);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-
-  db('fruits').where({ id }).first()
-    .then(fruit => {
-      res.json(fruit);
-    })
-    .catch(err => {
-      res.status(500).json({ message: 'Failed to retrieve fruit' });
-    });
+router.get("/:id", async (req, res, next) => {
+try {
+  const fruit = await Fruit.getBy(req.params.id)
+  res.json(fruit)
+} catch (error) {
+  next(error)
+}
 });
 
-router.post('/', (req, res) => {
-  const fruitData = req.body;
-  db('fruits').insert(fruitData)
-    .then(ids => {
-      db('fruits').where({ id: ids[0] })
-        .then(newFruitEntry => {
-          res.status(201).json(newFruitEntry);
-        });
-    })
-    .catch(err => {
-      console.log('POST error', err);
-      res.status(500).json({ message: "Failed to store data" });
-    });
+router.post("/",  async (req, res, next) => {
+  try {
+    const inserted = await Fruit.postNew(req.body)
+    res.status(201).json(inserted)
+  } catch(error) {
+    next(error)
+  }
 });
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -5,9 +5,16 @@ const fruitsRouter = require('./fruits/fruits-router.js');
 
 const server = express();
 
-server.use(helmet());
+// server.use(helmet());
 server.use(express.json());
 
 server.use('/api/fruits', fruitsRouter);
+
+server.use((err, req, res, next) => { //eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack,
+    })
+})
 
 module.exports = server;
